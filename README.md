fdd1eee68046427ac393aede96a193369679dcea
Sven <135715161+Joschy1976@users.noreply.github.com>
2025-01-17 07:07:22 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-# Portfolio-Vorlage
 ### Description
 
 A simple portfolio template for developer/designers built with React. 
 
-### [live preview comming](https://)
+### [live preview](https://ubaimutl.github.io/react-portfolio/)
 
-
+[![react portfoiio](src/assets/images/react%20portfolio%20gif.gif)](https://ubaimutl.github.io/react-portfolio/)
 
 ### Features
 
---
+++ src/content_option.js
@@ -0,0 +1,174 @@
+const logotext = "SVEN";
+const meta = {
+    title: "Sven Janorschke",
+    description: "Ich bin Sven Janorschke, Data Scientist und Full-Stack-Entwickler, derzeit tätig in Gevelsberg.",
+};
+
+const introdata = {
+    title: "Ich bin Sven Janorschke",
+    animated: {
+        first: "Ich liebe das Programmieren",
+        second: "Ich entwickle coole Webseiten",
+        third: "Ich entwickle mobile Apps, KI, Spiele und mehr.",
+    },
+    description: "Hier Ihren Text eintippen und speichern.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Etwas über mich!",
+    aboutme: "Hier Ihren Infotext tippen.",
+};
+const worktimeline = [{
+        jobtitle: "Designer der Woche",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer der Woche",
+        where: "Jamalya",
+        date: "2021",
+    },
+    {
+        jobtitle: "Designer der Woche",
+        where: "ALquds",
+        date: "2020",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "C++",
+        value: 97,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "C#",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 95,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Hier Ihren Infotext tippen.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Hier Ihren Infotext tippen.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Hier Ihren Infotext tippen.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "info@codeakademie-janorschke.de",
+    YOUR_FONE: "+491759272364",
+    description: "Hier Ihren Infitext tippen. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
